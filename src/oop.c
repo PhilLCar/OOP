dbe1705ab1fe4ad76aceb0b1203c23fdca359727
Philippe Caron <philippe-caron@hotmail.com>
2024-06-19 11:02:50 -0400

    OOP lib working#:wq

--- src/oop.c
+++ src/oop.c
@@ -11,14 +11,21 @@ void *_valloc(size_t size, const char *name)
 
 VirtualFunction _virtual(const char *name, const char *typename)
 {
-  struct _virtual_entry *v_table     = ({ extern struct _virtual_entry __start_virtual_table; &__start_virtual_table; });
-  struct _virtual_entry *v_table_end = ({ extern struct _virtual_entry __stop_virtual_table;  &__stop_virtual_table;  });
+  if (name && typename)
+  {
+    struct _virtual_entry *v_table     = ({ extern struct _virtual_entry __start_virtual_table; &__start_virtual_table; });
+    struct _virtual_entry *v_table_end = ({ extern struct _virtual_entry __stop_virtual_table;  &__stop_virtual_table;  });
 
-  for (struct _virtual_entry *ve = v_table; ve != v_table_end; ve++) {
-    if (strcmp(ve->object, typename)) continue;
-    if (strcmp(ve->method,     name)) continue;
+    // WARNING: This is most likely platform dependent
+    // Space between entries is the size aligned on 16
+    int space = (sizeof(struct _virtual_entry) + 0xF) & ~0xF;
 
-    return ve->fptr;
+    for (struct _virtual_entry *ve = v_table; ve != v_table_end; ve = (struct _virtual_entry*)((char*)ve + space)) {
+      if (strcmp(ve->object, typename)) continue;
+      if (strcmp(ve->method,     name)) continue;
+
+      return ve->fptr;
+    }
   }
   
   return NULL;
