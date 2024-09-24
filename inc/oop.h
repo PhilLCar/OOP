543f2ce1738aeb7e163986f9c21dee6fbf70977a
Philippe Caron <philippe-caron@hotmail.com>
2024-09-24 18:28:28 -0400

    Added the base macro for easy references to base objects

--- inc/oop.h
+++ inc/oop.h
@@ -40,6 +40,12 @@ struct EXPAND2(_, TYPENAME) {
 .fptr   = EXPAND3(TYPENAME, _, NAME) \
 }
 
+#define FOREIGN_VIRTUAL(NAME, FNAME) static struct _virtual_entry EXPAND2(_fe_, __COUNTER__) __attribute__((used, section("virtual_table"))) = { \
+.object = STRINGIZE(TYPENAME), \
+.method = #NAME, \
+.fptr   = FNAME \
+}
+
 // Creates a new instance of the object
 #define NEW(TYPENAME) TYPENAME ## _cons((TYPENAME*)_valloc(sizeof(TYPENAME), STRINGIZE(TYPENAME)) __NEW_CONT__
 #define __NEW_CONT__(...) __VA_OPT__(,)__VA_ARGS__)
@@ -52,6 +58,18 @@ if (VAR) { \
   free(VAR); \
   VAR = NULL; \
 }
+
+#define BASE_0 &_this->base
+#define BASE_1 BASE_0##.base
+#define BASE_2 BASE_1##.base
+#define BASE_3 BASE_2##.base
+#define BASE_4 BASE_3##.base
+#define BASE_5 BASE_4##.base
+#define BASE_6 BASE_5##.base
+#define BASE_7 BASE_6##.base
+#define BASE_8 BASE_7##.base
+#define BASE_9 BASE_8##.base
+#define BASE(N) EXPAND2(BASE_, N)
 
 // Since all members of the virtual entry are pointers, stack alignement should not be an issue
 struct _virtual_entry {
