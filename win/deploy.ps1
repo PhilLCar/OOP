cd565f50ebaf45785045a8cbe3a563f487d1bdb0
Philippe Caron <philippe-caron@hotmail.com>
2025-08-18 21:46:42 -0400

    update package

--- win/deploy.ps1
+++ win/deploy.ps1
@@ -3,15 +3,16 @@ param ($configuration)
 Write Host "Deploying $configuration..."
 
 if ($configuration -eq "Release") {
-  $nuspecFile = Get-ChildItem -Filter "*.nuspec" | Select-Object -First 1
+  $nugetDir   = "..\..\bin\$configuration"
+  $nuspecFile = Get-ChildItem ".." -Filter "*.nuspec" | Select-Object -First 1
   $newVersion = $([System.DateTime]::Now.ToString("yyyy.M.d.Hmm"))
 
   if ($nuspecFile) {
-      (Get-Content $nuspecFile.FullName) -replace '(<version>).*?(</version>)', "`$1 $newVersion `$2" | Set-Content $nuspecFile.FullName
-      Write-Host "Updated version to $newVersion in $($nuspecFile.Name)"
-	  rm    *.nupkg
-	  nuget pack
-	  nuget push *.nupkg -Source GitHub
+    (Get-Content $nuspecFile.FullName) -replace '(<version>).*?(</version>)', "`$1 $newVersion `$2" | Set-Content $nuspecFile.FullName
+    Write-Host "Updated version to $newVersion in $($nuspecFile.Name)"
+    rm    $nugetDir\*.nupkg -ErrorAction SilentlyContinue
+    nuget pack $nuspecFile.FullName -OutputDirectory $nugetDir
+    nuget push $nugetDir\*.nupkg -Source GitHub
   } else {
       Write-Host "No .nuspec file found."
   }
